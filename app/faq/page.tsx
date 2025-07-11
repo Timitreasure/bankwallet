0b98b0bb5cf468377e769540f2905a1319984574
Timitreasure <momonumbercompany2@gmail.com>
2025-07-11 11:32:30 +0100

    Update page.tsx

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -109,7 +109,7 @@ export default function FaqPage() {
             Our support team is available 24/7 to help you with any questions or issues you might have.
           </p>
           <a
-            href="https://t.me/OfficialBankwallet"
+            href="https://t.me/Bankwalletadmin"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center justify-center gap-2 w-full bg-black text-white py-2 rounded-md hover:bg-gray-900 transition-colors"
